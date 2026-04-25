6073cb82062ce7657f14e60798d14003eb35881a
ashwin sekaran <ashwin.sekaran@tc1.io>
2026-04-25 13:10:42 +0200

    Core game logic - place, isfull and winner logic

--- server/game.go
+++ server/game.go
@@ -22,3 +22,44 @@ func (b *board) display() string {
 	}
 	return result
 }
+func (b *board) place(row, col int, symbol string) bool {
+	if b[row][col] != "" {
+		return false
+	}
+	b[row][col] = symbol
+	return true
+}
+
+func (b *board) isFull() bool {
+	for _, row := range b {
+		for _, col := range row {
+			if col == "" {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func (b *board) winner() string {
+	lines := [8][3][2]int{
+		{{0, 0}, {0, 1}, {0, 2}},
+		{{1, 0}, {1, 1}, {1, 2}},
+		{{2, 0}, {2, 1}, {2, 2}},
+		{{0, 0}, {1, 0}, {2, 0}},
+		{{0, 1}, {1, 1}, {2, 1}},
+		{{0, 2}, {1, 2}, {2, 2}},
+		{{0, 0}, {1, 1}, {2, 2}},
+		{{0, 2}, {1, 1}, {2, 0}},
+	}
+	for _, line := range lines {
+		a := b[line[0][0]][line[0][1]]
+		b2 := b[line[1][0]][line[1][1]]
+		c := b[line[2][0]][line[2][1]]
+
+		if a != "" && a == b2 && b2 == c {
+			return a
+		}
+	}
+	return ""
+}
